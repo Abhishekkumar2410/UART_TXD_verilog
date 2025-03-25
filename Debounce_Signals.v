8e63c86341ebf205b7dee686da5cb60b0d6f178b
ABHISHEK KUMAR <136848489+Abhishekkumar2410@users.noreply.github.com>
2025-03-25 08:01:16 +0530

    Update Debounce_Signals.v

--- Debounce_Signals.v
+++ Debounce_Signals.v
@@ -33,8 +33,6 @@ module Debounce_Signals #(parameter threshold = 1000000)
     reg [25:0] count = 0; // 2 power 25, grreater than 12.5 million
     reg slow_clk = 0;
     
-  //  reg [30:0] count =0; //20 bits count for increment add decremnt wgen buttuon is pressed or released
-    
      
     //First use two FF synihronize the button signal "clk", clock domain
      
@@ -60,24 +58,9 @@ module Debounce_Signals #(parameter threshold = 1000000)
     end
     end
 
-    //when push button is pressed or released, we increamnt or decrement the counter
+
     
     
-//    always @(posedge clk)
-//    begin
-//    if(button_ff2) // if button_ff2 is high
-//    begin
-//    if(~&count) // if it isn't at the count limit, make sure you won't up at the limit. First AND all count and than not AND
-//    count <= count+1;
-//    end
-//    else begin
-//    if(|count) // if count has at least 1 in it, making sure subtarct when count is 0
-//    count <= count -1; // when btn released, count down
-//    end
-//    if (count > threshold) // if the count is than than the threshould
-//    transmit <= 1; // debounce signal is 1
-//    else
-//    transmit <= 0; // debonce signal is 0
-//    end
+
  
 endmodule
